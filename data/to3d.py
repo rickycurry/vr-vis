569d760ea6d96ca17a371dad0f2d43617ed13c53
Ricky <rickycurry@gmail.com>
2022-06-05 12:47:21 -0700

    Add several clustering algorithms' outputs to output.csv

--- data/to3d.py
+++ data/to3d.py
@@ -1,5 +1,5 @@
-from sklearn import decomposition
-from pandas import read_csv, DataFrame #, to_csv
+from sklearn import decomposition, cluster
+from pandas import read_csv, DataFrame, Series
 
 # assumes the labels are in the rightmost column
 dataframe = read_csv("./data/iris.csv", delimiter=',')
@@ -12,5 +12,31 @@ transformed_data = pca.fit_transform(dataframe)
 
 new_data = DataFrame(transformed_data)
 new_data = new_data.join(labels)
+
+# Run clustering algorithms on the transformed data
+# K-Means
+predictions = cluster.KMeans(n_clusters=3).fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='kmeans'))
+# Affinity propagation
+predictions = cluster.AffinityPropagation().fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='afprop'))
+# Mean shift
+predictions = cluster.MeanShift().fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='meanshift'))
+# Spectral
+predictions = cluster.SpectralClustering(n_clusters=3).fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='spectral'))
+# Hierarchical
+predictions = cluster.AgglomerativeClustering(n_clusters=3).fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='hierarchical'))
+# DBSCAN
+predictions = cluster.DBSCAN().fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='dbscan'))
+# OPTICS
+predictions = cluster.OPTICS().fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='optics'))
+# BIRCH
+predictions = cluster.Birch().fit_predict(transformed_data)
+new_data = new_data.join(Series(predictions, name='birch'))
 
 new_data.to_csv("./data/output.csv")
