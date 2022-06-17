90eb5c13189da253d495339b5aaafd5237a0043c
Ricky <rickycurry@gmail.com>
2022-06-17 08:40:20 -0700

    POST request sends all the data as JSON

--- server/rerunClustering.js
+++ server/rerunClustering.js
@@ -1,14 +1,25 @@
 async function rerunClustering() {
+    const children = document.getElementById('options').children;
+    let options = Array.from(children);
+    options = options.filter(d => d.tagName === 'SELECT' || d.tagName === 'INPUT');
+    let mapped = options.map(d => {
+        obj = {};
+        obj[d.id] = d.value;
+        return obj;
+    });
     const response = await fetch('http://localhost:8080', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            "ok": "my guy",
-            "cash_in": "cash_out"
-        })
-        // TODO: Pass arguments
+        body: JSON.stringify(
+            [
+                ...mapped,
+                {
+                    "meanshift-cluster-all": document.getElementById('meanshift-cluster-all').checked
+                }
+            ]
+        )
     });
     const text = await response.text();
     console.log(text);
